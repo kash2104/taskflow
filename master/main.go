f361cb3d5889fe4f75f5afeef57102e511c7ee6c
kash2104 <kparikh1104@gmail.com>
2025-10-06 18:55:08 +0530

    master: Main function initialise.

---
+++ master/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sync"
+
+	"github.com/joho/godotenv"
+	"github.com/kash2104/taskflow/master/db"
+	"github.com/kash2104/taskflow/master/server"
+)
+
+
+func main(){
+
+
+	fmt.Println("Starting the program")
+
+
+	err := godotenv.Load(".env");
+	if err != nil {
+		log.Fatalf("error while loading env")
+	}
+	MONGO_URI := os.Getenv("MONGODB_URI");
+
+	if err := db.Connect(MONGO_URI); err != nil{
+		log.Fatalf("error connecting db %v", err);
+	}
+
+	taskChannel := make(chan db.UserRequest, 10)	
+
+	var wg sync.WaitGroup;
+	wg.Add(2);
+
+	go func(){
+		fmt.Println("Starting 1st thread");
+		defer wg.Done();
+		server.StartServer(taskChannel);
+	}()
+
+	go func(){
+		fmt.Println("Starting 2nd thread");
+		defer wg.Done()
+		server.StartGRPCServer(taskChannel);
+	}()
+
+	wg.Wait();
+	close(taskChannel);
+
+}
