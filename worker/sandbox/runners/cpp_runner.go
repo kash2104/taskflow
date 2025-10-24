08beb52258d5d96fcdd049d0b27230da9ac218d9
kash2104 <kparikh1104@gmail.com>
2025-10-24 19:03:42 +0530

    sandbox: Added cpp runners.

---
+++ worker/sandbox/runners/cpp_runner.go
@@ -0,0 +1,55 @@
+package runners
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+
+	"github.com/kash2104/taskflow/worker/sandbox"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+var basePath string = "/var/local/lib/isolate/"
+
+func CompileCpp(taskId primitive.ObjectID, code string) error{
+	boxId:= ConvertNumberFromHex(taskId)
+	
+	// _, err := WriteCodeToSandbox(taskId,code,"cpp");
+
+	// if err != nil{
+	// 	return err;
+	// }
+
+	// compile code
+	// compile := exec.Command("isolate","--box-id="+boxId,"--run","--", "g++",basePath+boxId+"/box/main_"+boxId+".cpp", "-o", "main_"+boxId);
+
+	compile := exec.Command("g++", "main_" + boxId + ".cpp", "-o", "main_" + boxId);
+	compile.Dir = basePath + boxId + "/box"
+
+	if err := compile.Run(); err != nil{
+		return fmt.Errorf("compilation error!! %v", err);
+	}
+	return nil;
+}
+
+func RunCpp(taskId primitive.ObjectID) (string, string, error) {
+	boxId:= ConvertNumberFromHex(taskId)
+	
+
+	//run code
+	cmd := exec.Command("isolate", "--box-id="+boxId,"--time=4", "--run", "./main_"+boxId);
+	stdoutpipe,_ := cmd.StdoutPipe()
+	stderrpipe,_ := cmd.StderrPipe()
+
+	cmd.Start()
+
+	stdout, _ := io.ReadAll(stdoutpipe);
+	stderr,_ := io.ReadAll(stderrpipe);
+
+	err := cmd.Wait();
+
+	defer sandbox.CleanupSandbox(taskId);
+
+	return string(stdout), string(stderr), err;
+
+}
